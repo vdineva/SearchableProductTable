0cc6c77c7c18023fcdee93cfed701bfc59542714
Vanya Dineva <dineva_vanya@yahoo.com>
2017-01-19 16:08:03 -0500

    Some refactoring

--- src/components/SearchableProductTable.js
+++ src/components/SearchableProductTable.js
@@ -48,42 +48,53 @@ class SearchableProductTable extends Component {
     this.setState(this.initialState);
   }
 
-  filterProducts(property_value) {
-    let selectedProperty = this.props.properties.find(property => {
+  getSelectedProperty() {
+    return this.props.properties.find(property => {
       return property.name === this.state.filterProperty;
     });
+  }
+
+  filterProducts(property_value) {
+    let selectedProperty = this.getSelectedProperty();
 
     if (property_value.property_id !== selectedProperty.id) {
       return;
     }
 
+    const filterValue = this.state.filterValue;
+    const value = property_value.value;
+
     switch (this.state.filterOperator) {
       case 'contains':
       case 'any':
-        if (typeof this.state.filterValue === 'string') {
-          return (property_value.value.toLowerCase().includes(this.state.filterValue.toLowerCase()));
+        if (selectedProperty.type === 'string') {
+          return (value.toLowerCase().includes(filterValue.toLowerCase()));
         } else {
-          return (this.state.filterValue.includes(property_value.value));
+          return (this.state.enumeratedFilterValue.includes(value));
         }
       case 'in':
-        if (typeof this.state.filterValue === 'string') {
-          return (property_value.value.toLowerCase().includes(this.state.filterValue.toLowerCase()));
+        if (selectedProperty.type === 'string') {
+          return (value.toLowerCase().includes(filterValue.toLowerCase()));
         } else {
-          return (this.state.filterValue.indexOf(property_value.value) > -1);
+          return (this.state.enumeratedFilterValue.indexOf(value) > -1);
         }
       case 'none':
-        return (this.state.filterValue.indexOf(property_value.value) === -1);
+        if (selectedProperty.type === 'string') {
+          return (filterValue.indexOf(value) === -1);
+        } else {
+          return (this.state.enumeratedFilterValue.indexOf(value) === -1);
+        }
       case 'greater_than':
-        return (property_value.value > this.state.filterValue);
+        return (value > filterValue);
       case 'less_than':
-        return (property_value.value < this.state.filterValue);
+        return (value < filterValue);
       case 'equals':
         if (selectedProperty.type === 'string') {
-          return (property_value.value.toLowerCase() === this.state.filterValue.toLowerCase());
+          return (value.toLowerCase() === filterValue.toLowerCase());
         } else if (selectedProperty.type === 'number') {
-          return (property_value.value.toString() === this.state.filterValue);
+          return (value.toString() === filterValue);
         } else {
-          return (this.state.enumeratedFilterValue.includes(property_value.value));
+          return (this.state.enumeratedFilterValue.includes(value));
         }
       default:
         break;
@@ -92,22 +103,17 @@ class SearchableProductTable extends Component {
 
   render() {
     let products = this.props.products;
-    if (this.state.filterOperator.length && this.state.filterProperty.length && (this.state.filterValue.length || this.state.enumeratedFilterValue.length)) {
+    let properties = this.props.properties;
+    let operators = this.props.operators;
+
+    let selectedProperty = this.getSelectedProperty();
+    let selectedOperator = this.state.filterOperator.length;
+
+    if (selectedProperty && selectedOperator && (this.state.filterValue.length || this.state.enumeratedFilterValue.length)) {
       products = this.props.products.filter(product => {
         return product.property_values.filter(this.filterProducts).length;
       });
     }
-
-    let properties = this.props.properties;
-    let operators = this.props.operators;
-
-    let selectedProperty = this.props.properties.find((property) => {
-      return property.name === this.state.filterProperty;
-    });
-
-    let selectedOperator = this.props.operators.find((operator) => {
-      return operator.id === this.state.filterOperator;
-    });
 
     if (selectedProperty) {
       operators = this.props.operators.filter(op => {
